b6a345dc36230c6e139f592c77f09dbc28b29dbc
Aram Papikyan <167888669+VincentoChangretta@users.noreply.github.com>
2024-12-19 00:45:22 +0200

    MyCV

---
+++ img.js
@@ -0,0 +1,27 @@
+import myPhoto from "../src/images/Aram.webp";
+import reactLogo from "../src/images/react.svg";
+import reduxLogo from "../src/images/redux.svg"
+import tailwindLogo from "../src/images/tailwind.svg"
+import toolsHTML from "../src/images/html.png";
+import toolsCSS from "../src/images/css.png";
+import toolsSASS from "../src/images/sass.png";
+import toolsJS from "../src/images/js.png";
+import toolsREACT from "../src/images/react.png";
+import toolsPHP from "../src/images/php.png";
+
+export const toolsData = [
+  { id: "html", img: toolsHTML },
+  { id: "css", img: toolsCSS },
+  { id: "sass", img: toolsSASS },
+  { id: "js", img: toolsJS },
+  { id: "react", img: toolsREACT },
+  { id: "php", img: toolsPHP },
+];
+
+export {
+  myPhoto,
+  reactLogo,
+  reduxLogo,
+  tailwindLogo
+};
+
